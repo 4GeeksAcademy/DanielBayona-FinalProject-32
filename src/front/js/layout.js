72c1a015240c27aa9dfd5a3d7f18532183d594b4
leonardo munoz 🌑🌍 <168220131+leonardomunooz@users.noreply.github.com>
2024-08-26 21:56:09 +0000

    admin view sidebar layout is done, resposive is missing

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,27 +1,29 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
-import { BackendURL } from "./component/backendURL";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import ScrollToTop from "./component/scrollToTop";
+// import { BackendURL } from "./component/backendURL";
 
-import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
+// import { Home } from "./pages/home";
+// import { Demo } from "./pages/demo";
+// import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
+import Admin  from "./views/Admin.jsx"
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+// import { Navbar } from "./component/navbar";
+// import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
+    // const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    // if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
 
     return (
         <div>
-            <BrowserRouter basename={basename}>
+            <Admin />
+            {/* <BrowserRouter basename={basename}>
                 <ScrollToTop>
                     <Navbar />
                     <Routes>
@@ -32,7 +34,7 @@ const Layout = () => {
                     </Routes>
                     <Footer />
                 </ScrollToTop>
-            </BrowserRouter>
+            </BrowserRouter> */}
         </div>
     );
 };
---
+++ src/front/js/views/Admin.jsx
@@ -0,0 +1,16 @@
+import React  from "react";
+import Sidebar from "../component/Sidebar.jsx";
+
+const  Admin =  () => {
+    return (
+            <>
+                <Sidebar />
+            </>
+         
+            
+    )
+}
+
+
+
+export default Admin 
