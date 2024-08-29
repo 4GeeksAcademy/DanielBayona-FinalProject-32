9a3b578918845fe8f0d7254c45db8fdd402b15b8
Daniel Bayona <65182005+Bayunas@users.noreply.github.com>
2024-08-29 02:50:10 +0000

    Added the login view

--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -2,12 +2,15 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext.js";
 import WorkerNavBar from "../component/workerNavBar.js";
 import SuperVisorNavBar from "../component/supervisorNavBar.js";
+import Sidebar from "../component/Sidebar.jsx";
 import "../../styles/home.css";
 
 export const Home = () => {
   return (
     <>
-      <SuperVisorNavBar />
+      <WorkerNavBar />
     </>
   );
 };
+
+export default Home;
