a97195ed3c734951635e7e309fdc24296b3f95a3
leonardo munoz 🌑🌍 <168220131+leonardomunooz@users.noreply.github.com>
2024-09-06 20:48:19 +0000

    se ha aniadido el fetch para obtener usuarios

--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -33,7 +33,7 @@ const injectContext = (PassedComponent) => {
 
     // The initial value for the context is not null anymore, but the current state of this component,
     // the context will now have a getStore, getActions and setStore functions available, because they were declared
-    // on the state of this component
+    // on the state of this componen
     return (
       <Context.Provider value={state}>
         <PassedComponent {...props} />
